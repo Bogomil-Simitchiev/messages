c25d3c7ac93167a5fc76794741b87b323f9891a0
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-11-22 01:52:02 +0200

    Fixed e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { MessagesModule } from 'src/messages/messages.module';
+import { MessagesModule } from '../src/messages/messages.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -21,7 +21,7 @@ describe('AppController (e2e)', () => {
       { id: 2, text: 'NestJS is great!' },
     ];
     return request(app.getHttpServer())
-      .get('/')
+      .get('/messages')
       .expect(200)
       .expect(messages);
   });
