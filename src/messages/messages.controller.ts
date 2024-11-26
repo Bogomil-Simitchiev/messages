517800d9965778dda3a064bf8d42eb8a24905b59
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-11-27 00:57:35 +0200

    Took a look over dtos and pipes in NestJS

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Param, Res } from '@nestjs/common';
+import { Controller, Get, Post, Param, Res, Body } from '@nestjs/common';
 import { Response } from 'express';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,18 +9,19 @@ export class MessagesController {
         const messages = [
             { id: 1, text: 'Hello World' },
             { id: 2, text: 'NestJS is great!' },
-          ];
-      
-          res.status(200).json(messages);
+        ];
+
+        res.status(200).json(messages);
     }
 
     @Post()
-    createMessage(): void {
-      console.log('Create message!');
+    createMessage(@Body() body: CreateMessageDto): void {
+        console.log(body);
+
     }
 
     @Get('/:_id')
     getMessage(@Param() params: any): void {
-      console.log(params._id);
+        console.log(params._id);
     }
 }
